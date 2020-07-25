ad7b69c67e6a3ff5ca6a4139ae22ad2808ec7b88
Englert Ervin  Brunó <englert@users.noreply.github.com>
2020-07-25 19:06:21 +0200

    Update Snooker-oop-csupasz.py

--- Snooker-oop-csupasz.py
+++ Snooker-oop-csupasz.py
@@ -1,3 +1,5 @@
+# 2. _______________________________________________________________________
+
 class Snooker:
     def __init__(self, sor):
         s = sor.strip().split(';')
@@ -9,14 +11,20 @@ class Snooker:
 with open('snooker.txt', encoding='latin2') as f:
     fejléc = f.readline().strip().split(';')
     lista = [ Snooker(sor) for sor in f ]
+
 # 3. _______________________________________________________________________
+
 versenyzők_száma = len(lista)
 print(f'3. feladat: A világranglistán {versenyzők_száma} versenyző szerepel')
+
 # 4. ________________________________________________________________________
+
 bevétel_lista = [ sor.nyeremény for sor in lista ] 
 átlag = sum(bevétel_lista) / versenyzők_száma
 print(f'4. feladat: A versenyzők átlagosan {átlag:.2f} fontot kerestek')
+
 # 5. _______________________________________________________________________
+
 kínaiak = [ ( sor.nyeremény, sor) for sor in lista if sor.ország =='Kína']
 nyeremény, adatok = max(kínaiak)
 print(f'''
@@ -26,13 +34,17 @@ print(f'''
         Ország: {adatok.ország}
         Nyeremény összege: {adatok.nyeremény * 380} Ft
 ''')
+
 # 6. ________________________________________________________________________
+
 norvég = [sor for sor in lista if sor.ország == 'Norvégia']
 if len(norvég) > 0:
     print(f'6. feladat: A versenyzők között van norvég versenyző.')
 else:
     print(f'6. feladat: A versenyzők között nincs norvég versenyző.')
+
 #7. __________________________________________________________________________
+
 statisztika = dict()
 print(      f'7. feladat: Statisztika')
 for sor in lista:
